d690da26de13ec725e928ad57eff8dacf76784c4
reid gillis <pg27reid@vfs.com>
2025-04-12 01:14:32 -0700

    sync save with cloud

---
+++ Assets/Scripts/Saving/CloudSaving.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking; //used for making calls 
+
+
+public class CloudSaving : MonoBehaviour
+{
+    
+    public LocalSaveManager localSaveManager;                                       //Get instance of local save manager
+
+    private string serverUrl = "http://localhost:3000/syncLocalClientWithCloud";    //url that connects to POST in backend
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SyncWithCloud();
+        }
+    }
+
+    public void SyncWithCloud()
+    {
+        StartCoroutine(SyncRoutine());
+    }
+
+    private IEnumerator SyncRoutine()
+    {
+        
+        PlayerData localData = localSaveManager.playerData;             //Get local data. This is stored in the LocalSaveManager class's playerData object
+        string localJson = JsonUtility.ToJson(localData);               //Convert localData TO Json, stringifying it, and save it to var.
+
+        
+        WWWForm form = new WWWForm();                                   //Create new WWWForm object --> used to make the call
+
+        form.AddField("plainJson", localJson);                          //AddField --> add header. This is store in req body. - req.body.plainJson 
+
+        using (UnityWebRequest www = UnityWebRequest.Post(serverUrl, form))//make call
+        {
+            yield return www.SendWebRequest();                              //wait here until we get a request back
+            
+            if (www.result != UnityWebRequest.Result.Success)               //check --> if the result from the request is not successful
+            {
+                Debug.Log($"syncError : {www.error} ");
+            }
+            else
+            {
+                string serverResponse = www.downloadHandler.text;           //downloadHandler--> gets the response from the web request. This stringifies the JSON
+                                                                            //Convert the stringified JSON to an object.
+                                                                            //the repsonse, var serverResponse, and casting it into a PlayerData object
+                PlayerData serverData = JsonUtility.FromJson<PlayerData>(serverResponse);
+                Debug.Log(serverData);
+                localSaveManager.playerData = serverData;                   //SAVE --> change local data to the data received from server
+                localSaveManager.SaveToLocal();                             //take playerData we just overwrote and saves the data to the .js file
+
+                Debug.Log("save successul");
+            }
+        }
+    }
+}
